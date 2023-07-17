c3da269c0ce5121312130bf5173171d54f680adb
lisa_ywl <yourworthlesslife@icloud.com>
2023-07-17 15:07:53 +0300

    Hshs

--- detective/src/pages/Main/Main.jsx
+++ detective/src/pages/Main/Main.jsx
@@ -1,9 +1,11 @@
 import MainBG from "../../components/MainBG/MainBG.jsx";
+import Slider from "../../components/Slider/Slider.jsx";
 
 function Main() {
   return (
     <>
       <MainBG />
+      <Slider />
     </>
   );
 }
